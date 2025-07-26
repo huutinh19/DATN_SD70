e4a9f2cc663b263a518236ff1151b62f107dbf1a
huutinh19 <huutinh191204@gmail.com>
2025-07-26 11:31:58 +0700

    done

---
+++ back_end/src/main/java/com/poly/sport/infrastructure/common/GenCode.java
@@ -0,0 +1,38 @@
+package com.poly.sport.infrastructure.common;
+
+
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+import java.text.Normalizer;
+import java.util.UUID;
+import java.util.regex.Pattern;
+
+@Component
+public class GenCode {
+
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final int CODE_LENGTH = 10;
+    public static String genCodeByName(String name){
+        Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+        return pattern.matcher(Normalizer.normalize(name, Normalizer.Form.NFD))
+                .replaceAll("")
+                .replaceAll("\\s","").toUpperCase();
+    }
+
+    public static String randomPassword(){
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+    public static String randomCodeVoucher() {
+        SecureRandom secureRandom = new SecureRandom();
+        StringBuilder code = new StringBuilder(CODE_LENGTH);
+
+        for (int i = 0; i < CODE_LENGTH; i++) {
+            int randomIndex = secureRandom.nextInt(CHARACTERS.length());
+            char randomChar = CHARACTERS.charAt(randomIndex);
+            code.append(randomChar);
+        }
+
+        return code.toString();
+    }
+}
